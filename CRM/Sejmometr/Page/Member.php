75c46c051069f9de29128c0099ef7992d1fc0524
Michau <michal@civicrm.org>
2012-12-11 09:20:04 +0100

    testing data pull from Sejmometr

--- CRM/Sejmometr/Page/Member.php
+++ CRM/Sejmometr/Page/Member.php
@@ -10,7 +10,21 @@ class CRM_Sejmometr_Page_Member extends CRM_Core_Page {
 
     $member = $dataset->where( 'id', '=', '2')->find_one();
 
+    //CRM_Core_Error::debug( $member );
+    
     $this->assign('member', $member->data);
+
+    $associates = $member->wspolpracownicy()->find_all();
+    $tpl_associates = array(); 
+    foreach( $associates as $key => $associate) {
+      $tpl_associates[$key] = $associate->data;
+    }
+    $this->assign('associates', $tpl_associates);
+
+    $club = $member->klub();
+
+    // CRM_Core_Error::debug( $club );
+    // CRM_Core_Error::debug( $associates );
     
     parent::run();
   }
