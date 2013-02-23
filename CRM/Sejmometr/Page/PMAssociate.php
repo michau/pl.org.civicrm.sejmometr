47ee65485b4640d6fa4bf2aa168d6a91a3dccb2b
Michał Mach <mover@caltha.pl>
2013-02-23 13:46:06 +0100

    PM association done

--- CRM/Sejmometr/Page/PMAssociate.php
+++ CRM/Sejmometr/Page/PMAssociate.php
@@ -4,59 +4,72 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Sejmometr_Page_PMAssociate extends CRM_Core_Page {
 
-  function associateaction() {
-    $contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, true);
-    $sejmid = CRM_Utils_Request::retrieve('sejmid', 'Positive', $this, true);
-    
-    $result = civicrm_api('Contact', 'Update', array(
-      'id' => $contactId,
-      'external_identifier' => "$sejmid",
-      'version' => 3,
-    ));
-    
+    function associateaction() {
+        $contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, true);
+        $sejmid = CRM_Utils_Request::retrieve('sejmid', 'Positive', $this, true);
+
+        $result = civicrm_api('Contact', 'Update', array(
+            'id' => $contactId,
+            'external_identifier' => "$sejmid",
+            'version' => 3,
+                ));
+
         $contact = civicrm_api('Contact', 'Getsingle', array(
-      'contact_id' => $contactId,
-      'contact_type' => 'Individual',
-      'sequential' => 0,
-      'version' => 3,
-        ));
-    
-    var_dump($contact);
-    
-  }
+            'contact_id' => $contactId,
+            'contact_type' => 'Individual',
+            'sequential' => 0,
+            'version' => 3,
+                ));
 
-  function run() {
-    $this->_contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, true);
-
-    $this->assign('contactID', $this->_contactId);
-    $result = civicrm_api('Contact', 'Getsingle', array(
-      'contact_id' => $this->_contactId,
-      'contact_type' => 'Individual',
-      'sequential' => 0,
-      'version' => 3,
-        ));
-
-    $first_name = $result['first_name'];
-    $last_name = $result['last_name'];
-    $full_name = $first_name . ' ' . $last_name;
-    $this->assign('fullName', $full_name);
-
-    $dataset = new ep_Dataset('poslowie');
-    $members = $dataset->where('nazwa', '=', $full_name)->find_all();
-    if (empty($members)) {
-      $this->assign('notFound', TRUE);
-
-      $others_by_first_name = $dataset->where('imie_pierwsze', '=', $result['first_name'])->find_all();
-      $this->assign('othersByFirstName', $others_by_first_name);
-      $others_by_last_name = $dataset->where('nazwisko', '=', $result['last_name'])->find_all();
-      $this->assign('othersByLastName', $others_by_last_name);
-    }
-    else {
-      $this->assign('notFound', FALSE);
-      $this->assign('members', $members);
+        var_dump($contact);
     }
 
-    parent::run();
-  }
+    function run() {
+        $this->_contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, true);
+
+        $this->assign('contactID', $this->_contactId);
+        $result = civicrm_api('Contact', 'Getsingle', array(
+            'contact_id' => $this->_contactId,
+            'contact_type' => 'Individual',
+            'sequential' => 0,
+            'version' => 3,
+                ));
+
+        $first_name = $result['first_name'];
+        $last_name = $result['last_name'];
+        // in expected Sejmometr format
+        $full_name = $first_name . ' ' . $last_name;
+        
+        if (empty($last_name) && empty($first_name)) {
+            $this->assign('emptyName', TRUE);
+        } else {
+
+            $this->assign('fullName', $full_name);
+
+            // ask Sejmometr
+            $dataset = new ep_Dataset('poslowie');
+            $members = $dataset->where('nazwa', '=', $full_name)->find_all();
+            if (empty($members)) {
+                $this->assign('notFound', TRUE);
+                
+                if( ! empty($first_name)) {
+                    $others_by_first_name = $dataset->where('imie_pierwsze', '=', $result['first_name'])->find_all();
+                    $this->assign('othersByFirstName', $others_by_first_name);
+                } else {
+                    $this->assign('noOthersByFirstName', TRUE);
+                }
+                if( ! empty($last_name)) {
+                    $others_by_last_name = $dataset->where('nazwisko', '=', $result['last_name'])->find_all();
+                    $this->assign('othersByLastName', $others_by_last_name);
+                } else {
+                    $this->assign('noOthersByLastName', TRUE);
+                }
+            } else {
+                $this->assign('notFound', FALSE);
+                $this->assign('members', $members);
+            }
+        }
+        parent::run();
+    }
 
 }
--- templates/CRM/Sejmometr/Page/PMAssociate.tpl
+++ templates/CRM/Sejmometr/Page/PMAssociate.tpl
@@ -1,21 +1,45 @@
 <h3>Associate Sejmometr information with this record:</h3>
 
-{if $notFound}
-    {ts 1=$fullName}Could not find Parliament Members with the name being exactly: %1.{/ts}
-
-    {ts}Maybe you would like to choose from these other Members below:{/ts}
+{if $emptyName}
+    {ts}Seems like last name of your contact is empty. Please define at least last name in the contact record in order to be able to search through Sejmometr API.{/ts}
+{elseif $notFound}
+    <p>
+    {ts 1=$fullName}Could not find Parliament Members with name being exactly: %1.{/ts}
+    </p>
+    <p>
+    {ts}Maybe you would like to choose from these other Parliament Members below?{/ts}
+    </p>
+    {if not $noOthersByFirstName}
+        <p>
+        {ts}People in Parliament with the same first name:{/ts}
+        <ul>
+        {foreach from=$othersByFirstName item=otherFname}
+            <li>{$otherFname->data.nazwa} ({$otherFname->data.id})
+            - <a href="{crmURL p='civicrm/sejmometr/associateaction' q="cid=`$contactID`&sejmid=`$otherFname->data.id`"}">{ts}associate{/ts}</a>
+            </li>
+        {/foreach}
+        </ul>
+        </p>
+    {/if}
+    {if not $noOthersByLastName}
+        <p>
+        {ts}People in Parliament with the same last name:{/ts}
+        <ul>
+        {foreach from=$othersByLastName item=otherLname}
+            <li>{$otherLname->data.nazwa} ({$otherLname->data.id})
+            - <a href="{crmURL p='civicrm/sejmometr/associateaction' q="cid=`$contactID`&sejmid=`$otherLname->data.id`"}">{ts}associate{/ts}</a>            
+            </li>
+        {/foreach}
+        </ul>
+        </p>
+    {/if}
+{else}
+    {ts}There you go. Is it this person?{/ts}
     <ul>
-    {foreach from=$othersByFirstName item=otherFname}
-        <li>{$otherFname->data.nazwa} ({$otherFname->data.id})
-        - <a href="{crmURL p='civicrm/sejmometr/associateaction' q="cid=`$contactID`&sejmid=`$otherFname->data.id`"}">{ts}associate{/ts}</a>
+    {foreach from=$members item=member}
+        <li>{$member->data.nazwa} ({$member->data.id})
+            - <a href="{crmURL p='civicrm/sejmometr/associateaction' q="cid=`$contactID`&sejmid=`$member->data.id`"}">{ts}associate{/ts}</a>
         </li>
     {/foreach}
-    {foreach from=$othersByLastName item=otherLname}
-        <li>{$otherLname->data.nazwa}</li>
-    {/foreach}
     </ul>
-{else}
-    {foreach from=$members item=member}
-        <li>{$member->data.nazwa}</li>
-    {/foreach}
 {/if}
