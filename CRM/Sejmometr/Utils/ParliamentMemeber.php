b4c696c4ad391e34a692a3fd5c64d54a7c251ecc
Lukasz Urbanski <lukasz@caltha.pl>
2013-02-23 15:27:16 +0100

    ParliamentMemeber class definition.

---
+++ CRM/Sejmometr/Utils/ParliamentMemeber.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+/*
+ * ParliamentMemeber Class. 
+ * Implements eP_Posel class.
+ */
+class CRM_Sejmometr_Utils_ParliamentMemeber {
+
+  function __construct()
+  {
+
+  }  
+
+}
