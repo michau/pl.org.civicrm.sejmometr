3d6a51b95695cf5a386f7273291b0c075395466b
Michał Mach <mover@caltha.pl>
2013-02-25 00:39:06 +0100

    various cleanups and some more functionality getting in

--- sejmometr.php
+++ sejmometr.php
@@ -7,7 +7,7 @@ require_once('packages/ePF_API/ep_API.php');
  * Implementation of hook_civicrm_config
  */
 function sejmometr_civicrm_config(&$config) {
-  _sejmometr_civix_civicrm_config($config);
+    _sejmometr_civix_civicrm_config($config);
 }
 
 /**
@@ -16,35 +16,35 @@ function sejmometr_civicrm_config(&$config) {
  * @param $files array(string)
  */
 function sejmometr_civicrm_xmlMenu(&$files) {
-  _sejmometr_civix_civicrm_xmlMenu($files);
+    _sejmometr_civix_civicrm_xmlMenu($files);
 }
 
 /**
  * Implementation of hook_civicrm_install
  */
 function sejmometr_civicrm_install() {
-  return _sejmometr_civix_civicrm_install();
+    return _sejmometr_civix_civicrm_install();
 }
 
 /**
  * Implementation of hook_civicrm_uninstall
  */
 function sejmometr_civicrm_uninstall() {
-  return _sejmometr_civix_civicrm_uninstall();
+    return _sejmometr_civix_civicrm_uninstall();
 }
 
 /**
  * Implementation of hook_civicrm_enable
  */
 function sejmometr_civicrm_enable() {
-  return _sejmometr_civix_civicrm_enable();
+    return _sejmometr_civix_civicrm_enable();
 }
 
 /**
  * Implementation of hook_civicrm_disable
  */
 function sejmometr_civicrm_disable() {
-  return _sejmometr_civix_civicrm_disable();
+    return _sejmometr_civix_civicrm_disable();
 }
 
 /**
@@ -57,7 +57,7 @@ function sejmometr_civicrm_disable() {
  *                for 'enqueue', returns void
  */
 function sejmometr_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _sejmometr_civix_civicrm_upgrade($op, $queue);
+    return _sejmometr_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -67,7 +67,7 @@ function sejmometr_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  * is installed, disabled, uninstalled.
  */
 function sejmometr_civicrm_managed(&$entities) {
-  return _sejmometr_civix_civicrm_managed($entities);
+    return _sejmometr_civix_civicrm_managed($entities);
 }
 
 /**
@@ -77,29 +77,31 @@ function sejmometr_civicrm_managed(&$entities) {
  * is installed, disabled, uninstalled.
  */
 function sejmometr_civicrm_tabs(&$tabs, $contactID) {
-  
-  $contact = civicrm_api('Contact', 'Getsingle', array(
-    'contact_id' => $contactID,
-    'contact_type' => 'Individual',
-    'sequential' => 0,
-    'version' => 3,
-      ));
-  
- if( !sejmometr_validate_configuration() ) {
-    $url = CRM_Utils_System::url('civicrm/sejmometr/configurationinfo', 
-        "reset=1&snippet=1&force=1&cid=$contactID");
-  } elseif( empty($contact['external_identifier'])) {
-    $url = CRM_Utils_System::url('civicrm/sejmometr/associate', 
-        "reset=1&snippet=1&force=1&cid=$contactID");
-  } else {
-    $url = CRM_Utils_System::url('civicrm/sejmometr/parliamentmemberinfo', 
-        "reset=1&snippet=1&force=1&cid=$contactID");
-  }
-  
-  $tabs[] = array('id' => 'sejmometrTab',
-    'url' => $url,
-    'title' => 'Sejmometr',
-    'weight' => 300);
+
+    $contact = civicrm_api('Contact', 'Getsingle', array(
+        'contact_id' => $contactID,
+        'contact_type' => 'Individual',
+        'sequential' => 0,
+        'version' => 3,
+            ));
+
+    // TODO: This is draft, do it properly
+    $pmContactType = 'Posel';
+    
+    if (in_array($pmContactType, $contact['contact_sub_type'] )) {
+        if (!sejmometr_validate_configuration()) {
+            $url = CRM_Utils_System::url('civicrm/sejmometr/configurationinfo', "reset=1&snippet=1&force=1&cid=$contactID");
+        } elseif (empty($contact['external_identifier'])) {
+            $url = CRM_Utils_System::url('civicrm/sejmometr/associate', "reset=1&snippet=1&force=1&cid=$contactID");
+        } else {
+            $url = CRM_Utils_System::url('civicrm/sejmometr/parliamentmemberinfo', "reset=1&snippet=1&force=1&cid=$contactID");
+        }
+
+        $tabs[] = array('id' => 'sejmometrTab',
+            'url' => $url,
+            'title' => 'Sejmometr',
+            'weight' => 300);
+    }
 }
 
 /**
@@ -107,12 +109,12 @@ function sejmometr_civicrm_tabs(&$tabs, $contactID) {
  *
  */
 function sejmometr_validate_configuration() {
-    
-    if (!defined('eP_API_KEY') || eP_API_KEY == "" ) {
-       return false;
+
+    if (!defined('eP_API_KEY') || eP_API_KEY == "") {
+        return false;
     }
     if (!defined('eP_API_SECRET') || eP_API_SECRET == "") {
-       return false;     
+        return false;
     }
     return true;
 }
